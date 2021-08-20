c0be3322e3d29d231cffcad3b4f38046a73c810b
Lymanto Hadibrata <lymantohadibrata@Lymantos-MacBook-Pro.local>
2021-08-21 00:54:16 +0700

    first commit and slicing splash,signin,signup page

---
+++ lib/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+
+//0x = kode hexa
+//ff = opacity 100%
+
+Color primaryColor = Color(0xff6C5ECF);
+Color secondaryColor = Color(0xff38ABBE);
+Color alertColor = Color(0xffED6363);
+Color priceColor = Color(0xff2C96F1);
+Color backgroundColor1 = Color(0xff1F1D2B);
+Color backgroundColor2 = Color(0xff2B2937);
+Color backgroundColor3 = Color(0xff242231);
+Color primaryTextColor = Color(0xffF1F0F2);
+Color secondaryTextColor = Color(0xff999999);
+Color subTitleColor = Color(0xff504F5E);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryTextColor,
+);
+TextStyle subTitleTextStyle = GoogleFonts.poppins(
+  color: subTitleColor,
+);
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: priceColor,
+);
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
